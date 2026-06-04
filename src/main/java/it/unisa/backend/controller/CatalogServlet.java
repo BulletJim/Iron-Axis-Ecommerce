fb17c230484d2f9c47effd0c2a4feba27926ba78
BulletJim <gianmarco.campania@gmail.com>
2026-06-04 18:24:19 +0200

    Creata landing page e catalog.jsp. Creata piccola servlet di reindirizzamento base alla pagina del catalogo. Creato prototipo di catalogo con mockup. Rinominata nomenclatura delle cartelle e refattorizzata architettura dell'applicativo.

---
+++ src/main/java/it/unisa/backend/controller/CatalogServlet.java
@@ -0,0 +1,27 @@
+package it.unisa.backend.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CatalogServlet")
+public class CatalogServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/view/catalog.jsp").forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
